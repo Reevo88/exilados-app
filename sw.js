5b49a248370f81033d484326bb7bd0453dd2e4be
Reevo88 <mr.guima@gmail.com>
2026-05-24 19:58:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // EXILADOS DA BOLA - Service Worker (PWA)
 // ==========================================
 
-const CACHE_NAME = 'exilados-v62';
+const CACHE_NAME = 'exilados-v63';
 
 // Arquivos que ficam no cache para funcionar offline
 const STATIC_ASSETS = [
